db079704468d65c7f208a6e3b7891e92b1a6a41d
Harshal0902 <harshalraikwar07@gmail.com>
2022-07-17 00:22:45 +0530

    ✨ Add upload NFT page

--- client/src/container/Main.js
+++ client/src/container/Main.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from '../components/Navbar';
 import Home from '../pages/Home';
+import Upload from '../pages/Upload';
 
 function Main() {
 
@@ -9,6 +10,7 @@ function Main() {
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/upload" element={<Upload />} />
             </Routes>
         </BrowserRouter>
     );
